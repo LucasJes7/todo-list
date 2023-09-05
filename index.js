e650ffcb6203982fe2a0f1ef1cabe2a62f114362
lucas_santo <lucastyson8@gmail.com>
2023-09-05 15:59:29 -0300

    bugs correction

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
-function createCloseButton(li) {
+function createDeleteButton(li) {
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
 
   span.className = "close";
+  span.style.cursor = "pointer";
   span.appendChild(txt);
   li.appendChild(span);
 
   span.onclick = () => span.parentElement.style.display = "none";
 }
 
+function validateInput() {
+  const input = document.getElementById("task");
+  if( input.value == undefined || 
+      input.value == "" ||
+      input.value == " "
+  ) {
+    document.getElementById("addButton").disabled = true;
+  }
+  else {
+    document.getElementById("addButton").disabled = false;
 
+  }
+}
 
 function add() {
-  let li = document.createElement('LI');
-  let input_value = document.form_main.task.value;
-  let input_text = document.createTextNode(input_value);
+  const li = document.createElement('LI');
+  const input_value = document.form_main.task.value;
+  const input_text = document.createTextNode(input_value);
+
+  input_text.addEventListener('click', () => {
+      
+    if (input_text.style.textDecoration === 'line-through') {
+      
+      input_text.style.textDecoration = 'none';
+    } else {
+      
+      input_text.style.textDecoration = 'line-through';
+    }
+  });
 
   li.appendChild(input_text);
   document.querySelector('ul').appendChild(li);
   document.form_main.task.value = "";
 
-  createCloseButton(li);
+  createDeleteButton(li);
+  
+  document.getElementById("addButton").disabled = true;
 
-  const listItems = document.querySelectorAll('li');
+  //const listItems = document.querySelectorAll('li');
 
 
-listItems.forEach(item => {
-  item.addEventListener('click', () => {
-    
-    if (item.style.textDecoration === 'line-through') {
-      
-      item.style.textDecoration = 'none';
-    } else {
-      
-      item.style.textDecoration = 'line-through';
-    }
-  });
-});
+  
 }
 
